1398a53618fc4de0d0de5981ab2ccb532eb0617b
Diego Viana <diegovianaf7@gmail.com>
2023-02-09 13:35:35 -0300

    add navbar to navigate between the pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { Routes, Route } from 'react-router-dom'
-import { Container } from "react-bootstrap";
-import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { Store } from './pages/Store';
+import { Container } from "react-bootstrap"
+import { Home } from './pages/Home'
+import { About } from './pages/About'
+import { Store } from './pages/Store'
+import { Navbar } from './components/Navbar'
 
 function App() {
   return (
-    <Container className='mb-4'>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/store' element={<Store />} />
-      </Routes>
-    </Container>
+    <>
+      <Navbar />
+      <Container className='mb-4'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/store' element={<Store />} />
+        </Routes>
+      </Container>
+    </>
   )
 }
 
