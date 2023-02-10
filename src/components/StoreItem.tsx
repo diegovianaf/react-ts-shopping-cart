b945c09ec251bb5bb1c502a666362393c7b83941
Diego Viana <diegovianaf7@gmail.com>
2023-02-10 14:05:50 -0300

    add product name and price

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -16,6 +16,12 @@ export const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
         height='240px'
         style={{ objectFit: 'cover' }}
       />
+      <Card.Body className='d-flex flex-column'>
+        <Card.Title className='d-flex justify-content-between align-items-baseline'>
+          <span className='fs-2'>{name}</span>
+          <span className='ms-2 text-muted'>{price}</span>
+        </Card.Title>
+      </Card.Body>
     </Card>
   )
 }
